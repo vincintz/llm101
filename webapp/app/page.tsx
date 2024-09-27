3ca53e3ecf9f1f72f169fe800e5362524a11b8a8
Theo Reyes <vincintz@yahoo.com>
2024-09-27 23:20:10 +0800

    First commit

---
+++ webapp/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white">
+        Hello, world!
+      </div>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
